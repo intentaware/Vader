b17a48ede7b37670644bbcb7dbabd50edd1e1283
Shalini Roy <shaliniroy012@gmail.com>
2016-02-23 15:39:31 +0530

    Added some queries

--- docs/canada-census-data/testing.py
+++ docs/canada-census-data/testing.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 import psycopg2
 import csv
+from django.db import connections
 
 
 def GeoCode(conn):
@@ -81,7 +82,7 @@ def Topic(conn):
                 if idx == 0:
                     # for column in val:
                     #     pass
-                    query = "ALTER TABLE %s AADD COLUMN %s SERIAL PRIMARY KEY,ADD %s text UNIQUE;" % (tableName, 'topic_id', val[2])
+                    query = "ALTER TABLE %s ADD COLUMN %s SERIAL PRIMARY KEY,ADD %s text UNIQUE;" % (tableName, 'topic_id', val[2])
                     print query
                     try:
                         cur.execute(query)
@@ -135,50 +136,57 @@ def Characteristics(conn):
     else:
         print "Sorry Buddy, No database connection"
 
-# def GeoProfile(conn):
-#     if conn:
-#         conn.set_isolation_level(0)
-#         cur = conn.cursor()
-#         with open('/home/shalini/csv/shalini.CSV') as f:
-#             reader= csv.reader(f)
-#             tableName = 'Geo_Profile'
-#             cur.execute("CREATE TABLE IF NOT EXISTS %s();"  % (tableName))
-#             for idx, val in enumerate(reader):
-#                 if idx == 0:
-#                     # for column in val:
-#                     #     pass
-#                     query = "ALTER TABLE %s ADD COLUMN %s text REFERENCES GeoId (%s), ADD COLUMN %s text REFERENCES GeoId (%s);"
-#                              % (tableName, 'geoid', 'geoid', 'topic', 'topic')
-#                     print query
-#                     try:
-#                         cur.execute(query)
-#                     except psycopg2.Error as e:
-#                         print e.pgerror
-#                         pass
-#                 else:
-#                     val = [str(x) for x in val]
-#                     print val
-#                     val = val[2:8]
-#                     print val
-#                     val.insert(0, tableName)
-#                     print tuple(val)
-#                     query = "INSERT INTO %s VALUES ('%s')" % tuple(val)
-#                     try:
-#                         cur.execute(query)
-#                     except psycopg2.Error as e:
-#                         print e.pgerror
-#                         pass
-#     else:
-#         print "Sorry Buddy, No database connection"
+def GeoProfile(conn):
+    if conn:
+        conn.set_isolation_level(0)
+        cur = conn.cursor()
+        with open('/home/shalini/csv/shalini.CSV') as f:
+            reader= csv.reader(f)
+            tableName = 'Geo_Profile'
+            cur.execute("CREATE TABLE IF NOT EXISTS %s();"  % (tableName))
+            for idx, val in enumerate(reader):
+                if idx == 0:
+                    # for column in val:
+                    #     pass
+                    query = "ALTER TABLE %s ADD COLUMN %s text REFERENCES Geo_Code (%s), ADD COLUMN %s text REFERENCES Geo_Nom (%s), ADD COLUMN %s text REFERENCES Topic (%s), ADD COLUMN %s text REFERENCES Characteristic (%s);"
+                             % (tableName, 'geo_id', 'geo_id', 'nom_id', 'nom_id', 'topic_id', 'topic_id', 'char_id', 'char_id')
+                    print query
+                    try:
+                        cur.execute(query)
+                    except psycopg2.Error as e:
+                        print e.pgerror
+                        pass
+                    query = "ALTER TABLE %s ADD COLUMN %s integer, ADD COLUMN %s integer, ADD COLUMN %s integer, ADD COLUMN %s integer;"
+                             % (tableName, val[5], val[6], val[8], val[10])
+                    print query
+                    try:
+                        cur.execute(query)
+                    except psycopg2.Error as e:
+                        print e.pgerror
+                        pass
+                # else:
+                #     val = [str(x) for x in val]
+                #     print val
+                #     val = val[2:8]
+                #     print val
+                #     val.insert(0, tableName)
+                #     print tuple(val)
+                #     query = "INSERT INTO %s VALUES ('%s')" % tuple(val)
+                #     try:
+                #         cur.execute(query)
+                #     except psycopg2.Error as e:
+                #         print e.pgerror
+                #         pass
+    else:
+        print "Sorry Buddy, No database connection"
 
 if __name__ == '__main__':
     # try:
     #     conn=psycopg2.connect("dbname='shalini' user='root' password='shalini'")
     # except:
     #     conn = None
-    from django.conf import settings
     try:
-        conn=psycopg2.connect("dbname='%s' user='%s' password='%s' host='%s' port='%s'" % (settings.US_CENSUS_DB['NAME'], 
+        conn=psycopg2.connect("dbname='ca_census' user='%s' password='%s' host='%s' port='%s'" % ( 
         settings.US_CENSUS_DB['USER'], settings.US_CENSUS_DB['PASSWORD'], settings.US_CENSUS_DB['HOST'],
         settings.US_CENSUS_DB['PORT']))
     except psycopg2.Error as e:
