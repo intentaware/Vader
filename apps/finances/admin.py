b84ddf78400525a19329051031da227b79bde15e
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-02-26 04:30:46 +0500

    updated modules with fixtures

--- apps/finances/admin.py
+++ apps/finances/admin.py
@@ -6,7 +6,10 @@ from .models import *
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Invoice._meta.fields]
 
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'segment']
+
 admin.site.register(Invoice, InvoiceAdmin)
-admin.site.register(Module)
+admin.site.register(Module, ModuleAdmin)
 admin.site.register(Plan)
 admin.site.register(PlanModule)
