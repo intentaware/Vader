d07918c8b6fb2cd01a0207afa63bd5b3cc83ea08
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-02-28 11:15:19 +0500

    test another approach

---
+++ plugins/census/management/commands/ca_import.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.db import connections
+from django.core.management.base import BaseCommand, CommandError
+import csv, os
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+        path_to_file = os.path.join(settings.BASE_DIR, 'docs', 'test.csv')
+        cursor = connections['us_census'].cursor()
+
+        with open(path_to_file, 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            header = reader.next()
+            for row in reader:
+                #                     WHERE NOT EXISTS
+                #        (SELECT 1 FROM {table} WHERE geocode = '{geocode}');
+                query = """
+                    INSERT INTO
+                        {table} (geocode, province, city)
+                    SELECT
+                        '{geocode}', '{province}', '{city}'
+                    """.format(
+                            table = 'cacensus2011.geocodes',
+                            geocode=row[0],
+                            province=row[1],
+                            city=row[2]
+                        )
+                try:
+                    cursor.execute(query)
+                except:
+                    print query
