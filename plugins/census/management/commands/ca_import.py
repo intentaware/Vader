06c3587f30eca5680e72e4ac342fd363de92480a
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-03-01 05:41:54 +0500

    income data

--- plugins/census/management/commands/ca_import.py
+++ plugins/census/management/commands/ca_import.py
@@ -11,86 +11,91 @@ class Command(BaseCommand):
         income = os.path.join(settings.BASE_DIR, 'docs', 'income.csv')
         cursor = connections['us_census'].cursor()
 
-        with codecs.open(population, 'rb', encoding='utf-8', errors='ignore') as csvfile:
-            print 'importing population data'
-            table = 'cacensus2011.geocodes'
-            pop_table = 'cacensus2011.population'
-            print 'Droping and Creating Tables'
-            cursor.execute(
-                """
-                    DROP TABLE
-                        IF EXISTS {table};
-                    CREATE TABLE {table} (
-                        id BIGSERIAL PRIMARY KEY UNIQUE,
-                        geocode BIGINT NOT NULL UNIQUE,
-                        province TEXT NOT NULL,
-                        city TEXT NOT NULL
-                    );
-                    DROP TABLE
-                        IF EXISTS {pop_table};
-                    CREATE TABLE {pop_table} (
-                        id BIGSERIAL PRIMARY KEY UNIQUE,
-                        geocode BIGINT NOT NULL,
-                        topic TEXT NOT NULL,
-                        characteristics TEXT NOT NULL,
-                        total DECIMAL NOT NULL,
-                        male DECIMAL,
-                        female DECIMAL
-                    )
-                """.format(table=table, pop_table=pop_table)
-                )
-            print 'Drop and Creating Table Done'
-            print 'Reading CSV file'
+        # with codecs.open(population, 'rb', encoding='utf-8', errors='ignore') as csvfile:
+        #     print 'importing population data'
+        #     table = 'cacensus2011.geocodes'
+        #     pop_table = 'cacensus2011.population'
+        #     print 'Droping and Creating Tables'
+        #     cursor.execute(
+        #         """
+        #             DROP TABLE
+        #                 IF EXISTS {table};
+        #             CREATE TABLE {table} (
+        #                 id BIGSERIAL PRIMARY KEY UNIQUE,
+        #                 geocode BIGINT NOT NULL UNIQUE,
+        #                 province TEXT NOT NULL,
+        #                 city TEXT NOT NULL
+        #             );
+        #             DROP TABLE
+        #                 IF EXISTS {pop_table};
+        #             CREATE TABLE {pop_table} (
+        #                 id BIGSERIAL PRIMARY KEY UNIQUE,
+        #                 geocode BIGINT NOT NULL,
+        #                 topic TEXT NOT NULL,
+        #                 characteristics TEXT NOT NULL,
+        #                 total DECIMAL NOT NULL,
+        #                 male DECIMAL,
+        #                 female DECIMAL
+        #             )
+        #         """.format(table=table, pop_table=pop_table)
+        #         )
+        #     print 'Drop and Creating Table Done'
+        #     print 'Reading CSV file'
+        #     reader = csv.reader(csvfile)
+        #     header = reader.next()
+        #     for row in reader:
+        #         # inserting geocodes
+        #         query = """
+        #             SELECT * FROM {table} WHERE geocode='{geocode}';
+        #         """.format(table=table, geocode=row[0])
+        #         cursor.execute(query)
+        #         result = len(cursor.fetchall())
+        #         if result == 0:
+        #             print 'Inserting Geocode into Table'
+        #             query = """
+        #                 INSERT INTO
+        #                     {table} (geocode, province, city)
+        #                 SELECT
+        #                     '{geocode}', '{province}', '{city}'
+        #                 """.format(
+        #                         table = table,
+        #                         geocode=row[0],
+        #                         province=row[1],
+        #                         city=row[2]
+        #                     )
+        #             try:
+        #                 cursor.execute(query)
+        #             except IntegrityError as e:
+        #                 print e
+        #             except DataError as e:
+        #                 print row
+        #                 print e
+
+        #         # inserting actual data
+        #         print 'Inserting actual data'
+        #         query = """
+        #             INSERT INTO
+        #                 {pop_table} (geocode, topic, characteristics, total, male, female)
+        #             SELECT
+        #                 '{geocode}', '{topic}', '{characteristics}', '{total}', '{male}', '{female}'
+        #         """.format(
+        #                 pop_table=pop_table,
+        #                 geocode=row[0],
+        #                 topic=row[3],
+        #                 characteristics=row[4],
+        #                 total=row[6] or 0,
+        #                 male=row[8] or 0,
+        #                 female=row[10] or 0
+        #             )
+        #         try:
+        #             cursor.execute(query)
+        #         except DataError as e:
+        #             print row
+        #             print e
+
+        with codecs.open(income, 'rb', encoding='utf-8', errors='ignore') as csvfile:
+            print 'importing income data'
             reader = csv.reader(csvfile)
             header = reader.next()
             for row in reader:
-                # inserting geocodes
-                query = """
-                    SELECT * FROM {table} WHERE geocode='{geocode}';
-                """.format(table=table, geocode=row[0])
-                cursor.execute(query)
-                result = len(cursor.fetchall())
-                if result == 0:
-                    print 'Inserting Geocode into Table'
-                    query = """
-                        INSERT INTO
-                            {table} (geocode, province, city)
-                        SELECT
-                            '{geocode}', '{province}', '{city}'
-                        """.format(
-                                table = table,
-                                geocode=row[0],
-                                province=row[1],
-                                city=row[2]
-                            )
-                    try:
-                        cursor.execute(query)
-                    except IntegrityError as e:
-                        print e
-                    except DataError as e:
-                        print row
-                        print e
-
-                # inserting actual data
-                print 'Inserting actual data'
-                query = """
-                    INSERT INTO
-                        {pop_table} (geocode, topic, characteristics, total, male, female)
-                    SELECT
-                        '{geocode}', '{topic}', '{characteristics}', '{total}', '{male}', '{female}'
-                """.format(
-                        pop_table=pop_table,
-                        geocode=row[0],
-                        topic=row[3],
-                        characteristics=row[4],
-                        total=row[6] or 0,
-                        male=row[8] or 0,
-                        female=row[10] or 0
-                    )
-                try:
-                    cursor.execute(query)
-                except DataError as e:
-                    print row
-                    print e
-
-        with codecs.open(filename)
+                print row
