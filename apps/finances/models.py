1c30d74b4d3033796bc89c92c94fc8720cdfb9e5
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-03-15 23:46:43 +0500

    minor mapping changed

--- apps/finances/models.py
+++ apps/finances/models.py
@@ -136,6 +136,8 @@ class Plan(TimeStamped, Stripe):
                     self.create_stripe_plan()
             except self._stripe.error.InvalidRequestError:
                 self.create_stripe_plan()
+        else:
+            self.create_stripe_plan()
         return super(Plan, self).save(*args, **kwargs)
 
     def create_stripe_plan(self, *args, **kwargs):
