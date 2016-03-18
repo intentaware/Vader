1295c52d698ee65fe0bafd5d873fc167993d4c03
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-03-18 15:24:29 +0500

    handling stripe 500

--- apps/finances/api/views.py
+++ apps/finances/api/views.py
@@ -6,26 +6,38 @@ from apps.companies.models import CompanySubscription
 from apps.finances.models import Invoice
 from django.utils import timezone as _tz
 
+
 @api_view(['GET', 'POST'])
 def invoice_webhook(request):
     data = request.data
     if 'payment_succeeded' in data['type']:
         invoice = data['data']['object']
         # subscription = invoice['subscription']
-        subscription = CompanySubscription.objects.get(stripe_id=invoice['subscription'])
+        try:
+            subscription = CompanySubscription.objects.get(
+                stripe_id=invoice['subscription']
+            )
+        except CompanySubscription.DoesNotExist:
+            subscription = None
         # amount from stripe comes as type(str) and is in cents, we need to convert it to
         # type(Decimal) and dollars.cents
-        amount = Decimal[invoice['total']]/100
+        amount = Decimal[invoice['total']] / 100
         now = _tz.now()
-        Invoice.objects.create(
+        if subscription:
+            Invoice.objects.create(
                 stripe_id=invoice['id'],
-                company = subscription.company,
-                amount = amount,
-                attempts = invoice['attempt_count'],
-                attempted_on = now,
-                charged_on = now,
-                gateway_response = data,
-                is_paid = True
+                company=subscription.company,
+                amount=amount,
+                attempts=invoice['attempt_count'],
+                attempted_on=now,
+                charged_on=now,
+                gateway_response=data,
+                is_paid=True
             )
+            return Response({}, status=201)
+        else:
+            return Response({
+                    'message': 'No subscription for this subscription exists'
+                }, status=200)
 
     return Response({}, status=200)
