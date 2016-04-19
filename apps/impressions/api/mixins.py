fc542a632c9cc0cab84996bcf300da7539f8eb9c
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-04-19 19:57:20 +0500

    smarter lookup with ipstore involved

--- apps/impressions/api/mixins.py
+++ apps/impressions/api/mixins.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from ipware.ip import get_real_ip
+from googlemaps import Client
 
 from geoip2 import database, webservice
 
@@ -63,20 +64,84 @@ class BaseImpression(APIView):
                 doc[k.lower()] = v
         return doc
 
+    def get_from_ipstore(self, ip, postcode):
+        """
+        Gets the census data from IPStore if the postal code exists
+
+        Args:
+            ip (string): IP v4 in the format xxx.xxx.xxx.xxx
+            postcode (string): Long format postal code
+
+        Returns:
+            census (dict): dictionary object containing census data
+        """
+        census = None
+
+        try:
+            ipstore = IPStore.objects.get(ip=ip)
+        except:
+            queryset = IPStore.objects.filter(
+                geocoded_postal_code=postcode,
+                census__isnull=False
+            )
+            if queryset.count():
+                ipstore = queryset[0]
+            else:
+                ipstore = None
+
+        if ipstore:
+            census = ipstore.census
+
+        return census
+
     def get_US_census(self, postcode):
-        pass
+        geoid = Geography.objects.get(
+            full_name__contains=postcode
+        ).full_geoid.replace('|', '00US')
+        return CensusUS(geoid=geoid).computed_profile()
 
-    def get_CA_census(self, postcode, city):
-        pass
+    def get_CA_census(self, postcode, city, location, ip):
+        gmaps = Client(key=settings.GOOGLE_GEOCODE_KEY)
+        results = gmaps.reverse_geocode(
+            (location['latitude'], location['longitude'])
+        )
 
-    def get_census_data(self, country, postcode, city=None):
+        for r in results[0]['address_components']:
+            try:
+                types = r['types']
+                if types[0] == 'locality' and types[1] == 'political':
+                    city = r['long_name']
+                    if city == 'Mississauga':
+                        city = 'Toronto'
+
+                if types[0] == 'postal_code':
+                    postcode = r['long_name']
+            except:
+                pass
+
+        # first try from IPStore, else lookup census database and update the
+        #  ipstore in the process
+        census = self.get_from_ipstore(ip, postcode)
+        if not census:
+            census = CaCensus(city=city).get_profile()
+            IPStore.objects.create(
+                ip=ip,
+                census=census,
+                geocode=results,
+                geocoded_postal_code=postcode
+            )
+        return census
+
+    def get_census_data(self, country, postcode, city, location, ip):
         """
         Call the respective API to accumulate the cencus data
 
         Args:
-            country (string):   country iso code, for example CA for Canada, US for United States
-            postcode (string):  the postal code for the the place for which the census data is required
+            country (string): country iso code, for example CA for Canada, US for United States
+            postcode (string): the postal code for the the place for which the census data is required
             city (None, optional): city, as an option for fallback if the  postcode data is not available.
+            location (None, optional): dictionary with latitude and longitude as keys.
+            ip (None, optional): IP v4 Address in the format xxx.xxx.xxx.xxx where xxx is always less than 256
 
         Returns:
             census (dict): census dictionary object
@@ -85,7 +150,7 @@ class BaseImpression(APIView):
         if country == 'US':
             census = self.get_US_census(postcode)
         if country == 'CA':
-            census = self.get_CA_census(postcode, city)
+            census = self.get_CA_census(postcode, city, location, ip)
         return census
 
     def process_request(self, request):
@@ -110,8 +175,11 @@ class BaseImpression(APIView):
             country = ip2geo['country']['iso_code']
             city = ip2geo['city']['names']['en']
             postcode = ip2geo['postal']['code']
+            location = ip2geo['location']
 
         else:
             country = city = postcode = None
 
-        doc['census'] = self.get_census_data(country, postcode, city)
+        doc['census'] = self.get_census_data(
+            country, postcode, city, location, ip
+        )
